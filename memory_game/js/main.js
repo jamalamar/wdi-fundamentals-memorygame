ee648acd66b4469df25f2e9d83ec3718cefa11c6
Jamal Amar <JamalAmar@Jamals-MacBook-Air.local>
2018-12-04 12:27:43 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,23 @@
-console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTwo');
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	} else {
+	alert("Sorry, try again.");
+	}
+};
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function (cardId) {
+	
+	cardsInPlay.push(cards[cardId]);
+	
+	console.log("User Flipped" + " " + cards[cardId])	
 
-if (cardsInPlay.length !== 2) {
-alert("Pick 2 Cards");
-} else if (cardsInPlay[0] === cardsInPlay[1]) {
-alert("You found a match!");
-} else if (cardsInPlay[0] !== cardsInPlay[1]) {
-alert("Sorry, try again.");
-}
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(1);
